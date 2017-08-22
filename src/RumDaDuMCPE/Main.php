b2f81a240661959036c860f852a6c41df6f08e43
RumBenCraftPE <ameensahal321@gmail.com>
2017-08-22 13:13:53 +0200

    Plugin complete.

---
+++ src/RumDaDuMCPE/Main.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace RumDaDuMCPE\CombatHUD;
+
+class CombatHUD extends \pocketmine\plugin\PluginBase implements \pocketmine\event\Listener {
+
+	public function onEnable() {
+		$this->getServer()->getPluginManager()->registerEvents($this, $this);
+		$this->getServer()->getLogger()->info("§7[§aCOMBATHUD§7] §l§bEnabled!");
+	}
+
+	/**
+	 * @priority HIGH
+	 */
+
+	public function onMove(\pocketmine\event\player\PlayerMoveEvent $event) {
+		if ($this->playerIsInCombat($event->getPlayer())) {
+			$this->sendHUD($this->player);
+		}
+	}
+
+	public function playerIsInCombat(\pocketmine\Player $player) : bool {
+		$cl = $this->getServer()->getPluginManager()->getPlugin("CombatLogger");
+		if ($cl->isTagged($player)) return true;
+		return false;
+	}
+
+	public function sendHUD(\pocketmine\Player $player) {
+		$cl = $this->getServer()->getPluginManager()->getPlugin("CombatLogger");
+		$timeleft = $cl->getTagDuration($player);
+		$player->sendPopup(
+					"§l§cYou are now engaged in combat!\n".
+					"§r§bTime remaining: §a".$timeleft
+				  );
+	}
+}
