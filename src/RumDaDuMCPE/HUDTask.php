e8cf923a6579f462b7e1b638755fa6af05de8b14
iZeaoGamer <jameshayward14@icloud.com>
2020-03-08 12:49:54 +0000

    Players can no longer fly whilst in combat.

--- src/RumDaDuMCPE/HUDTask.php
+++ src/RumDaDuMCPE/HUDTask.php
@@ -3,12 +3,18 @@
 namespace RumDaDuMCPE;
 
 use pocketmine\scheduler\Task;
+use pocketmine\utils\TextFormat;
 
 class HUDTask extends Task {
 	public function onRun (int $currentTick) {
 		$plugin = CombatHUD::getInstance();
 		foreach ($plugin->getServer()->getOnlinePlayers() as $player) {
 			if ($plugin->PlayerisInCombat($player)) {
+				if($player->getAllowFlight() && !$player->isCreative()){
+					$player->setAllowFlight(false);
+					$player->setFlying(false);
+					$player->sendMessage(TextFormat::colorize("&cYou cannot fly whilst in combat. Disabled your flight."));
+				}
 				$player->sendPopup($plugin->sendHUD($player));
 			} else {
 				return;
