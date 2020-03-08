a7a7860e8fe0fe01f2738900cc3977b22057ea3d
iZeaoGamer <jameshayward14@icloud.com>
2020-03-08 13:21:10 +0000

    Implement functional Creative mode checks.

--- src/RumDaDuMCPE/HUDTask.php
+++ src/RumDaDuMCPE/HUDTask.php
@@ -4,19 +4,27 @@ namespace RumDaDuMCPE;
 
 use pocketmine\scheduler\Task;
 use pocketmine\utils\TextFormat;
+use pocketmine\Player;
 
 class HUDTask extends Task {
 	public function onRun (int $currentTick) {
 		$plugin = CombatHUD::getInstance();
 		foreach ($plugin->getServer()->getOnlinePlayers() as $player) {
 			if ($plugin->PlayerisInCombat($player)) {
-				if($player->getAllowFlight() && $plugin->CreativeCheck()){
+				if(!$plugin->CreativeCheck($player)){
+$plugin->setCreativeCheck($player, true);
+				}else{
+				if($player->getAllowFlight()){
 					$player->setAllowFlight(false);
 					$player->setFlying(false);
+					$player->setGamemode(Player::SURVIVAL);
 					$player->sendMessage(TextFormat::colorize("&cYou cannot fly whilst in combat. Disabled your flight."));
 				}
 				$player->sendPopup($plugin->sendHUD($player));
 			} else {
+					if($plugin->CreativeCheck($player)){
+					$plugin->setCreativeCheck($player, false);
+					}
 				return;
 			}
 
